4b27307ea2ef69a706c519d4a0f279290cc25e32
LeeJiyoung <cjstk7597@gmail.com>
2025-03-13 21:26:39 +0900

    [Feat] 스크렙터블 아이템 데이터 이미지 할당
    데이터 설정, 변수 이름 수정

--- Assets/02.Script/ScriptableObject/Item/ItemData.cs
+++ Assets/02.Script/ScriptableObject/Item/ItemData.cs
@@ -7,7 +7,8 @@ public class ItemData : ScriptableObject
     public string Description; //아이템 설명
     public Sprite Icon; //아이템 이미지
     public ItemType ItemType; // 아이템 타입
-    public int MaxHaveCount; //최대 소지수
+    public int CellMaxHaveCount; //인벤토리 슬롯 한칸 최대 소지수
+    public GameObject ItemPrefab; //아이템 프리펩 오브젝트
 }
 
 
