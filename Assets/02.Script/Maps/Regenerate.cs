0a607c1362bb75841d4cd4ca93e2062b32755224
YataGarasu8 <zmffldjdnld12@naver.com>
2025-03-13 17:55:46 +0900

    [Feat]리젠 영역 기즈모

--- Assets/02.Script/Maps/Regenerate.cs
+++ Assets/02.Script/Maps/Regenerate.cs
@@ -6,18 +6,57 @@ public class Regenerate : MonoBehaviour
 {
     [Header("RegenerateArea")]
     [SerializeField] private List<Bounds> regenerateAreaA;
+    [SerializeField] private List<Bounds> regenerateAreaB;
+    [SerializeField] private List<Bounds> regenerateAreaC;
+    [SerializeField] private List<Bounds> regenerateAreaD;
     [SerializeField] private List<GameObject> regenerateResource;
-    [SerializeField] private Color gizmoColor =new Color(1,0,0,0.3f);
+
+    public int resourceAmountA;
+    public int resourceAmountB;
+    public int resourceAmountC;
+    public int resourceAmountD;
+
+    void Respwon()
+    {
+
+    }
 
     private void OnDrawGizmosSelected()
     {
-        if(regenerateAreaA == null)
+        if (regenerateAreaA == null)
         {
             return;
         }
-        Gizmos.color = gizmoColor;
-        foreach(var area in regenerateAreaA)
+        if (regenerateAreaB == null)
         {
+            return;
+        }
+        if (regenerateAreaC == null)
+        {
+            return;
+        }
+        if (regenerateAreaD == null)
+        {
+            return;
+        }
+        foreach (var area in regenerateAreaA)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(area.center, area.size);
+        }
+        foreach (var area in regenerateAreaB)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireCube(area.center, area.size);
+        }
+        foreach (var area in regenerateAreaC)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireCube(area.center, area.size);
+        }
+        foreach (var area in regenerateAreaD)
+        {
+            Gizmos.color = Color.yellow;
             Gizmos.DrawWireCube(area.center, area.size);
         }
     }
