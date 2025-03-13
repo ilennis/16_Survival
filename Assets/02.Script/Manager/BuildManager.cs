a5d278724031f09c6bd657bf59117820ddc46f6d
Sinneli <nyeliplays1911@gmail.com>
2025-03-13 12:02:06 +0900

    일단 빌딩 메니저 코드

---
+++ Assets/02.Script/Manager/BuildManager.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildManager : MonoBehaviour
+{
+    public Camera playerCamera;
+    public GameObject[] buildingPrefabs; // 빌딩 Prefab. 리스트
+    public GameObject placementIndicator; // 가능하면 구현. 투명한 모양으로 빌딩 건설 Preview
+    public LayerMask groundLayerMask; // 땅에 Raycast 닿으면 구현
+    public int selectedBuildingIndex = 0;
+    public float maxPlacementDistance = 5f;
+    public Text errorText;
+
+    private GameObject currentPreview;
+    private bool canPlace = false; // true 여야지 된다.
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 문제: 어디에다가 해야지 이 버튼을 누르면 이렇게 들어가는가 정의하지?
+    }
+
+    void AimBuilding()
+    {
+        // Raycast로 카메라 조준하는 점 확인.
+        Ray ray = playerCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2)); // 스크린 한가운데
+        RaycastHit hit;
+
+        // Raycast에 맞으면 설치할 위치 표기하기. 
+        if (Physics.Raycast(ray, out hit, maxPlacementDistance, groundLayerMask))
+        {
+            if (currentPreview == null)
+            {
+                currentPreview = Instantiate(placementIndicator);
+            }
+
+            currentPreview.transform.position = hit.point;
+            canPlace = !Physics.CheckBox(hit.point, currentPreview.transform.localScale / 2); // Check for collisions
+        }
+        else // 해당 사항 안되면 Preview 없에기 (Raycast가 땅에 닿지 않을때)
+        {
+            canPlace = false;
+            if (currentPreview != null)
+            {
+                Destroy(currentPreview);
+            }
+        }
+
+    }
+    void PlaceBuilding()
+    {
+
+    }
+
+    bool CheckResources(int buildingIndex)
+    {
+        // 인벤토리 확인 용도.
+        return true;
+    }
+    bool DeductResources(int buildingIndex)
+    {
+        // 인벤토리에서 재료 빼기 용도.
+        return true;
+    }
+}
