152395f78cf33ab6e090a0cac959be1319020802
dduwls <azs1719@naver.com>
2025-03-13 10:54:22 +0900

    [Feat] 플레이어 이동 구현

--- Assets/02.Script/Player/Player.cs
+++ Assets/02.Script/Player/Player.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public PlayerController controller;
     private void Awake()
     {
         GameManager.Instance.Player = this;
+        CharacterManager.Instance.Player = this;
+        controller = GetComponent<PlayerController>();
     }
 }
